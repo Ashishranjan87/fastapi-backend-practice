eabb178a35f7e676617dbdd538a1550d8fd784f0
Ashish Ranjan <53873460+Ashishranjan87@users.noreply.github.com>
2026-05-26 03:24:29 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 # SQLALCHEMY_DATABASE_URI = "sqlite:///./todosapp.db"
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:root@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URI = "postgresql://todoapplicationdatabase_gjr2_user:neNaDrujDKY1MpLQo4a6TM2TIMub5uu5@dpg-d8ac8oul51nc73ck8png-a/todoapplicationdatabase_gjr2"
 # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/TodoApplicationDatabase"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
